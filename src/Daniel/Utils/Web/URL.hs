885b08e37d96f883caf6b306c7620edb2876b019
Daniel Harper <djharperuk@gmail.com>
2011-07-14 22:18:06 +0100

    New useless functions

--- src/Daniel/Utils/Web/URL.hs
+++ src/Daniel/Utils/Web/URL.hs
@@ -1,13 +1,31 @@
 module Daniel.Utils.Web.URL (
-    getParams
+    getParams,
+    getBaseURL,
+    getTLD,
+    getJavaOrgPackagePrefix
 )
 where
 
 import Daniel.Utils.List
+import Data.List (stripPrefix, intersperse, insert)
 
 type URL = String
 
 getParams :: URL -> [(String,String)]
 getParams u = map (\[key,val] -> (key,val)) $ map (split '=') $ split '&' $ tail $ dropWhile (/='?') u
 
+getBaseURL :: URL -> URL
+getBaseURL u = case (stripPrefix "http://" u) of
+                Just x -> takeWhile (/='/') x
+                Nothing -> takeWhile (/='/') u
 
+getTLD :: URL -> String
+getTLD u = last $ split '.' $ getBaseURL u
+
+-- ostentatious superficial corporate bullshit
+getJavaOrgPackagePrefix :: URL -> String
+getJavaOrgPackagePrefix u = case (stripPrefix "www." (getBaseURL u)) of
+                                Just x ->  java x
+                                Nothing -> java (getBaseURL u)
+                                where
+                                    java t = concat $ reverse $ insert "." $ reverse $ intersperse "." $ reverse $ split '.' t
